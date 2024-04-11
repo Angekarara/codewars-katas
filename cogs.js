80c09fd33e77c2c7567fa4f8cf2e9f66dec4a00f
Angekarara <mbabange2020@gmail.com>
2024-04-11 19:58:45 +0200

    new changes

--- cogs.js
+++ cogs.js
@@ -1,8 +1,12 @@
 function cogRpm(cogs, n) {
-  var s = 1;
-  for (var i = n; i > 0; --i) s *= -cogs[i] / cogs[i - 1];
-  var d = 1;
-  for (var i = n; i < cogs.length - 1; ++i) d *= -cogs[i] / cogs[i + 1];
+  let s = 1;
+  for (let i = n; i > 0; --i) {
+    s *= -cogs[i] / cogs[i - 1];
+  }
+  let d = 1;
+  for (let i = n; i < cogs.length - 1; ++i) {
+    d *= -cogs[i] / cogs[i + 1];
+  }
   return [s, d];
 }
 console.log(cogRpm([100, 50, 25], 0));
