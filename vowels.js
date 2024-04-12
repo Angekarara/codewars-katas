d745b9b8d39dad2c2f51c921c51c08d26d269dfb
Angekarara <mbabange2020@gmail.com>
2024-04-12 11:52:08 +0200

    counting vowels excluding y

---
+++ vowels.js
@@ -0,0 +1,11 @@
+function getCount(str) {
+  let count = 0;
+  let vowels = "aeiou";
+  let str2 = str.split("");
+  for (let s of str2) {
+    if (vowels.includes(s)) {
+      count += 1;
+    }
+  }
+  return count;
+}
