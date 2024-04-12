53837abcaaa088335b8720bca3581131ce48d4a6
Angekarara <mbabange2020@gmail.com>
2024-04-12 10:47:42 +0200

    sorting objects

--- sorting.js
+++ sorting.js
@@ -1,3 +1,11 @@
 function sortList(sortBy, list) {
   return list.sort((a, b) => b[sortBy] - a[sortBy]);
 }
+const list = [
+  { id: 1, value: 10 },
+  { id: 2, value: 5 },
+  { id: 3, value: 15 },
+  { id: 4, value: 7 },
+];
+
+console.log(sortList("value", list));
