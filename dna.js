3ba912bbd8172d7a42a4d9bab1e0a0569cc8876a
Angekarara <mbabange2020@gmail.com>
2024-05-07 11:07:47 +0200

    Complementary DNA

---
+++ dna.js
@@ -0,0 +1,16 @@
+function dnaStrand(dna) {
+  let dna1 = dna.split("");
+
+  let results = dna1.map((dn) => {
+    if (dn === "A") {
+      return dn.replace("A", "T");
+    } else if (dn === "G") {
+      return dn.replace("G", "C");
+    } else if (dn === "T") {
+      return dn.replace("T", "A");
+    } else if (dn === "C") {
+      return dn.replace("C", "G");
+    }
+  });
+  return results.join("");
+}
