ee32355ee68415c108e0d036d0b9cf441f1ba1ce
Angekarara <mbabange2020@gmail.com>
2024-05-08 11:45:56 +0200

    Simple Fun #179: Fraction

---
+++ simpleFunFraction.js
@@ -0,0 +1,16 @@
+function fraction(a, b) {
+  //coding and coding..
+  let smaller = Math.min(a, b);
+
+  let gcf = 0;
+  for (i = 1; i <= smaller; i++) {
+    if (a % i === 0 && b % i === 0) {
+      gcf = i;
+    }
+  }
+
+  const num1 = a / gcf;
+  const num2 = b / gcf;
+  const sum = num1 + num2;
+  return sum;
+}
