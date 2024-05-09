1756bd33c771e1d80057342306fda9e8c2aa8902
Angekarara <mbabange2020@gmail.com>
2024-05-09 11:10:45 +0200

    sum of numbers kata

---
+++ sumOfNumbers.js
@@ -0,0 +1,13 @@
+function getSum(a, b) {
+  let big = Math.max(a, b);
+
+  let small = Math.min(a, b);
+  let sum = 0;
+  if (a !== b) {
+    for (let i = small; i <= big; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+  return a;
+}
